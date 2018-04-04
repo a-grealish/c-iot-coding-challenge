0f08347a7fec557cc166d9149e686a8f45f0038d
Ashley Grealish <ashley@grealish.co.uk>
2018-04-04 09:49:37 +0100

    Adding k-means prototype to the data generation file

--- data/generate_test_data.py
+++ data/generate_test_data.py
@@ -2,7 +2,7 @@ import random as rd
 import numpy as np
 import matplotlib.pyplot as plt
 
-ittrs = 1000000
+ittrs = 100000
 on_count = 0
 lines = 0
 
@@ -26,3 +26,62 @@ print (on_count / lines)
 
 # plt.plot(values)
 # plt.show()
+
+## -------------- Do k-means clustering on the values ------------------------------
+print("Calculate k-means")
+
+ks = 2
+means = []
+
+# Forgy Init - Choose random point from the value array
+for k in range(0, ks):
+    means.append(values[rd.randrange(0, lines, 1)])
+
+print "Init means: ", means
+
+prev_means = []
+
+k_sums = [0 for x in range(0, ks)]
+k_count = [0 for x in range(0, ks)]
+
+i = 0
+
+while (1):
+
+    prev_means = means[:]
+
+    # For each val assign to a k
+    for val in values:
+        closest_k = 0
+        closest_k_dist = abs(val - means[0])
+        for k in range(1, ks):
+            if abs(val - means[k]) < closest_k_dist:
+                closest_k_dist = (val - means[k])
+                closest_k = k
+
+        # Add the value to the sum for that closest k and increment the count
+        k_sums[closest_k] += val
+        k_count[closest_k] += 1
+
+    # Calculate the new means
+    for k in range(0, ks):
+        # print k_sums[k], k_count[k]
+        if k_count[k] != 0:
+            means[k] = k_sums[k] / k_count[k]
+    
+    # Check if the means have converged
+    converged = True
+    for k in range(0, ks):
+        if abs(prev_means[k] - means[k]) > 0.00001:
+            converged = False
+    if converged:
+        print("Means converged")
+        break
+
+    # Max 50 itterations
+    i += 1
+    if (i > 20):
+        print("Reached ittr limit")
+        break
+
+print "Final means: ", means
